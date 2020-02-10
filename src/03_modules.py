8c7c8bf72a683d755dd2374072586004acc486ab
ropeks <ropeksmilioner@gmail.com>
2020-02-10 15:59:04 +0100

    do modules excercise

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,23 +9,30 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+args = sys.argv
+for arg in args:
+    print(arg)
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+platform = sys.platform
+print(platform)
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
+py_version = 'Python ' + sys.version[:5]
+print(py_version)
 
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+curr_pid = os.getpid()
+print(curr_pid)
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+cwd = os.getcwd()
+print(cwd)
 
 # Print out your machine's login name
-# YOUR CODE HERE
+login_name = os.getlogin()
+print(login_name)
